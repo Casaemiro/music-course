3727d5db33e63597ad584355c566fa0990934132
Casaemiro <silasmagho@gmail.com>
2022-07-03 19:50:14 +0100

    updates

--- src/About.js
+++ src/About.js
@@ -12,7 +12,10 @@ const About = () => {
                     About Us
                 </div>
                 <p>
-                Our platform is totally free for you to use! Pass your ticketing fees onto your buyers and put that money right back into your event. We waive all fees for free events and have extra low fees for tickets priced $5 and below.
+                Based in buea Cameroon, with clients amd team members all over the globe, we are all 
+about helping event organizers create, issue, track tickets and manage payments of their 
+events tickets. We also help individuals easily get tickets to their favorite events online 
+from the confort of their home.
                 </p>
             </div>
         </div>
@@ -23,7 +26,12 @@ const About = () => {
                     How we Operate
                 </div>
                 <p>
-                Our platform is totally free for you to use! Pass your ticketing fees onto your buyers and put that money right back into your event. We waive all fees for free events and have extra low fees for tickets priced $5 and below.
+                With 3 years of experience in helping both event organizers create, issue, track tickets 
+and handle payments, as well as 
+help individuals easily get tickets to their favorite events, conferences, seminars, festivals,
+etc. 
+We’ve grown, build productive teams and made the most of the available resources to 
+better serve our clients.
                 </p>
             </div>
             <img src="https://media.istockphoto.com/photos/high-angle-view-close-up-asian-woman-using-mobile-app-cinema-movie-picture-id1324465035?b=1&k=20&m=1324465035&s=170667a&w=0&h=FsfpHKvaNR05oit_Q96jO-0biZzt5Hzq4GitrCTqzMg=" alt="" style={{borderTopRightRadius:"3px",borderBottomRightRadius:"3px", height:"50%",marginBottom:"0px"}} className="about-image"/>
